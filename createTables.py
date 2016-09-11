4e70096295fb629b82e15b7ca630acf4eebe9394
Markus Broecker <broecker@wisc.edu>
2016-09-10 21:02:03 -0500

    Began work on external result analyzer

---
+++ createTables.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+# The MIT License (MIT)
+# Copyright (c) 2016 Markus Broecker <mbrckr@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights 
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+# copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+class Result:
+	def __init__(self, data):
+		self.name = data[0]
+		self.tgtSunk = data[1]
+		self.tgtTons = data[2]
+		self.subsDamaged = data[3]
+		self.subsSunk = data[4]
+		self.subsSpotted = data[5]
+		self.subsRTB =data[6]
+		self.subsPromoted = data[7]
+
+
+	def printSummary(self):
+		print('Sub',self.name, 'sunk', self.tgtSunk, 'ships for', self.tgtTons, 'tons. Status:', self.subsDamaged, 'damaged', self.subsSpotted, 'spotted', self.subsSunk, 'sunk', self.subsRTB, 'RTB', 'promoted', self.subsPromoted)
+
+
+
+
+
+def loadFile(filename):
+	result = []
+
+	file = open(filename, 'r')
+	for line in file:
+		toks = line.split(',')
+
+		if toks[0][0] == '#':
+			continue
+
+		for i in range(1,len(toks)):
+			toks[i] = int(toks[i])
+
+		r = Result(toks)
+		result.append(r)
+
+	return result
+
+
+
+if __name__ == '__main__':
+	data = loadFile('c1-wp1.csv')
+
+	for r in data:
+		r.printSummary()
+
+	count = len(data)
+
+	print('Subs')
+	print('-----------------------')
+
+	damaged = sum(r.subsDamaged for r in data if r.subsDamaged > 0)
+	print('Damaged : %4d'  % damaged + '/' + str(count))
+
+	sunk = sum(r.subsSunk for r in data if r.subsSunk > 0)
+	print('Sunk    : %4d' % sunk + '/' + str(count))
+
+	rtb = sum(r.subsRTB for r in data if r.subsRTB > 0)
+	print('RTB     : %4d' % rtb + '/' + str(count))
+
+	spotted = sum(r.subsSpotted for r in data if r.subsSpotted > 0)
+	print('Spotted : %4d' % spotted + '/' + str(count))
+	
+	promoted = sum(r.subsPromoted for r in data if r.subsPromoted > 0)
+	print('Promoted: %4d' %promoted + '/' + str(count))
